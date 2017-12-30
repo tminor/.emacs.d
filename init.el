9a877ddbd2720642075b6ee1db6563ca74b35be0
Thomas Minor <tminor@fixits-Air.internal.tminor.io>
2017-12-30 18:59:54 -0500

    added quality of life packages: ivy, ace-window, etc.

--- init.el
+++ init.el
@@ -1,17 +1,15 @@
 ;; General configuration
 (setq inhibit-startup-message t)
-
 ;; Explanation here https://emacs.stackexchange.com/questions/22717/what-does-require-package-mean-for-emacs-and-how-does-it-differ-from-load-fil
 (require 'package)
-
 ;; https://www.reddit.com/r/emacs/comments/1rdstn/set_packageenableatstartup_to_nil_for_slightly/
 (package-initialize)
 (setq package-enable-at-startup nil)
-
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 
-
+;; Enable linum-mode universally.
+(global-linum-mode 1)
 
 ;; For modularizing emacs configuration
 ;; from https://stackoverflow.com/questions/2079095/how-to-modularize-an-emacs-configuration
@@ -21,35 +19,34 @@
         ((boundp 'user-init-directory)
          user-init-directory)
         (t "~/.emacs.d/")))
-
 (defun load-user-file (file)
   (interactive "f")
   "Load a file in current user's configuration directory"
   (load-file (expand-file-name file user-init-dir)))
 
-
-
 ;; From http://cestlaz.github.io/posts/using-emacs-1-setup/#.Wb03a44pCfU
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
-	(package-refresh-contents)
-	(package-install 'use-package))
-
-
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 ;; List of included lisp files.
 ;; See the each file for explanatory comments.
 (load-user-file "packages.el")
 (load-user-file "mail.el")
 
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (auctex elpy use-package evil))))
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("5e52ce58f51827619d27131be3e3936593c9c7f9f9f9d6b33227be6331bf9881" default)))
+ '(package-selected-packages
+   (quote
+    (ivy ace-window framemove zenburn-theme auctex elpy use-package evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- packages.el
+++ packages.el
@@ -1,28 +1,60 @@
 ;; This file specifies what packages
-;; use-package should manage.
+;;   use-package should manage.
+;;
 ;; More here: https://github.com/jwiegley/use-package
+;;
+;; As package configuration starts to get
+;;   much longer, I'll probably move it to
+;;   its own *.el file.
 
+;; Evil mode stuff.
 (use-package evil
   :ensure t
   :config
   (evil-mode 1))
 
 ;; From http://cestlaz.github.io/posts/using-emacs-1-setup/#.Wb03a44pCfU
-;; Facilitates trying a package before installation
+;; Facilitates trying a package before installation.
 (use-package try
   :ensure t)
 
 ;; From http://cestlaz.github.io/posts/using-emacs-1-setup/#.Wb03a44pCfU
 ;; Provides key combination completion suggestions
-;; when a pause occurs after the first key in a sequence.
+;;   when a pause occurs after the first key in a sequence.
 (use-package which-key
-	:ensure t 
-	:config
-	(which-key-mode))
+  :ensure t 
+  :config
+  (which-key-mode))
 
 ;; From http://cestlaz.github.io/posts/using-emacs-2-org/#.Wb1EQY4pCfU
-;; Use bullets instead of asterisks
+;; Use bullets instead of asterisks.
 (use-package org-bullets
   :ensure t
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+;; Window navigation stuff.
+(use-package ace-window
+  :ensure t
+  :config
+  ;; ace-window meta key, change window keys to home row
+  (global-set-key (kbd "M-p") 'ace-window)
+  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
+
+;; ido-mode stuff.
+;; Good reference: https://www.masteringemacs.org/article/introduction-to-ido-mode
+;; (use-package ido
+;;   :ensure t
+;;   :config
+;;   (setq ido-enable-flex-matching t)
+;;   (setq ido-everywhere t)
+;;   (ido-mode 1))
+
+;; Neat guide here:
+;; https://writequit.org/denver-emacs/presentations/2017-04-11-ivy.html
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t
+            ivy-count-format "%d/%d "))
