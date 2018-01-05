19d9691a0427d99552a274415f52edf638f09cea
Thomas Minor <tminor@dhcp-82-214.uso.bor.usg.edu>
2018-01-05 17:12:13 -0500

    various things including global TODO states

--- init.el
+++ init.el
@@ -17,13 +17,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (zenburn)))
+ '(custom-enabled-themes (quote (material)))
  '(custom-safe-themes
    (quote
-    ("bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "5e52ce58f51827619d27131be3e3936593c9c7f9f9f9d6b33227be6331bf9881" default)))
+    ("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "be0b6c632aca938e4fae0cef244f50516d207d186408eee29692577bda29e6cf" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "5e52ce58f51827619d27131be3e3936593c9c7f9f9f9d6b33227be6331bf9881" default)))
+ '(nrepl-message-colors
+   (quote
+    ("#FF5454" "#F29000" "#FFFA87" "#00CF16" "#73FF82" "#00EBAA" "#A9FFF9" "#9A00CD")))
  '(package-selected-packages
    (quote
-    (hc-zenburn-theme ivy ace-window framemove zenburn-theme auctex elpy use-package evil))))
+    (magit material-theme rainbow-mode hc-zenburn-theme ivy ace-window framemove zenburn-theme auctex elpy use-package evil)))
+ '(pdf-view-midnight-colors (quote ("#E6E3E8" . "#2C2C2C"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
