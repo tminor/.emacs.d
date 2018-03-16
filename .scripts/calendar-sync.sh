d2d487f67a16effa037c629ce60b7ecba7a15a27
tminor <minort@gmail.com>
2018-03-16 16:56:29 -0400

    Changed path to awk script.

--- .scripts/calendar-sync.sh
+++ .scripts/calendar-sync.sh
@@ -2,7 +2,7 @@
 
 # customize these
 WGET='/usr/local/bin/wget'
-ICS2ORG='/Users/tminor/.emacs.d/ical2org.awk'
+ICS2ORG='/Users/tminor/.emacs.d/.scripts/ical2org.awk'
 ICSFILE='/Users/tminor/org/calendar.ics'
 ORGFILE='/Users/tminor/org/work/calendar.org'
 URL='https://calendar.google.com/calendar/ical/thomas.minor%40usg.edu/private-472001384d103e16ce6d093865d66a2c/basic.ics'
